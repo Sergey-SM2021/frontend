a19827eeca73fa06f2d71566c494f1db5c8ec02a
Sergey-SM2021 <sergey2003.k.96@gmail.com>
2023-05-14 22:41:51 +0300

    setup react testing library

--- src/widgets/sidebar/ui/Sidebar.test.tsx
+++ src/widgets/sidebar/ui/Sidebar.test.tsx
@@ -1,9 +1,16 @@
 import { render, screen } from "@testing-library/react"
 import { Sidebar } from "./Sidebar"
+import { I18nextProvider, withTranslation } from "react-i18next"
+import i18n from "./i18nTestConfig"
 
 describe("sidebar", () => {
 	it("render", () => {
-		render(<Sidebar />)
+		const WithTranslationSidebar = withTranslation()(Sidebar)
+		render(
+			<I18nextProvider i18n={i18n}>
+				<WithTranslationSidebar />
+			</I18nextProvider>
+		)
 		expect(screen.getByText("toggle")).toBeInTheDocument()
 	})
 })
