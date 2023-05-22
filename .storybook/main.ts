2618e8465f277fd1f4efd3a8a60ffc031326415f
Sergey-SM2021 <sergey2003.k.96@gmail.com>
2023-05-22 23:21:51 +0300

    setup story book

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,3 +1,4 @@
+import path from "path";
 import type { StorybookConfig } from "@storybook/react-webpack5";
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
@@ -12,6 +13,18 @@ const config: StorybookConfig = {
   },
   docs: {
     autodocs: "tag",
+  },
+  webpackFinal: (config) => {
+    config.resolve?.modules?.push(path.resolve("src"));
+    config.module?.rules?.push({
+      test: /\.scss$/,
+      use: [
+        "style-loader",
+        { loader: "css-loader", options: { modules: true } },
+        "sass-loader",
+      ],
+    });
+    return config;
   },
 };
 export default config;
